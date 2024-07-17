77e8c20fb502af99d351ed8261658bc5d95cab15
zikodotcom <foot.ball.american123@gmail.com>
2024-07-17 19:29:32 +0100

    Change the database information

--- connexion.js
+++ connexion.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'quotesdb'
+    host: 'sql304.infinityfree.com',
+    user: 'if0_36857789',
+    password: 'a0Nym6S8g2SUfg',
+    database: 'if0_36857789_quotesDB'
   })
   
   module.exports = connection;
